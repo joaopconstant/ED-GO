78e2b687256cd4c2b6b7d319677f3f4df47c6da6
Constant <110848773+joaopconstant@users.noreply.github.com>
2023-08-09 16:37:47 -0300

    Update ac1.go

--- ac1.go
+++ ac1.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(e_primo(6))
+	// fmt.Println(e_primo(6))
+
+	// fmt.Println(fibo(4))
+
+	// dia_semana()
+
+	// fmt.Println(e_bissexto(1995))
 }
 
 func e_primo(n int) bool {
@@ -19,19 +25,30 @@ func e_primo(n int) bool {
 	return primo
 }
 
-func fibo(num int) {
-	// cria um array com a serie de Fibonacci
-
-	// verifica o número de índice 'num'
+func fibo(n int) int {
+	var fibAnterior = 0
+	var fibAtual = 1
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else {
+		for i := 2; i <= n; i++ {
+			fibProximo := fibAnterior + fibAtual
+			fibAnterior = fibAtual
+			fibAtual = fibProximo
+		}
+		return fibAtual
+	}
 }
 
 func dia_semana() string {
-	// lê o valor (msg)
 	var msg string
 	var n int
+
 	print("Informe o número do dia da semana (de 1 a 7):")
 	fmt.Scanln(&n)
-	// relaciona números com dias
+
 	switch n {
 	case 1:
 		msg = "Domingo"
@@ -62,6 +79,14 @@ func dia_semana() string {
 	return msg
 }
 
-func e_bissexto() {
+func e_bissexto(n int) bool {
+	bissexto := false
 
+	if n % 4 == 0 && n % 100 != 0 {
+		bissexto = true
+	} else if n % 400 == 0 {
+		bissexto = true
+	}
+
+	return bissexto
 }
